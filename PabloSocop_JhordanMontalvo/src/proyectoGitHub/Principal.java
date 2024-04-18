e35afc06986d13c459e2b86425f959e52a195f1c
vboxuser <emailprogramador2@entornos.com>
2024-04-18 17:23:45 +0200

    Metodo suma prog2

--- PabloSocop_JhordanMontalvo/src/proyectoGitHub/Principal.java
+++ PabloSocop_JhordanMontalvo/src/proyectoGitHub/Principal.java
@@ -3,7 +3,12 @@ package proyectoGitHub;
 public class Principal {
 
 	public static void main(String[] args) {
+		double num1=12;
+		double num2=15;
 		
+		Programado2 metodo1=new Programado2();
+		
+		System.out.println(metodo1.suma(num1, num2));
 	}
 
 }
