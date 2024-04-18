b8ca9663eb228272a87a97b5d0e6452dc8670505
jhord <emailprogramador2@entornos.com>
2024-04-18 17:18:11 +0200

    Commit Eclipse desde el Programador1 para subir a GitHub

--- PabloSocop_JhordanMontalvo/src/proyectoGitHub/Principal.java
+++ PabloSocop_JhordanMontalvo/src/proyectoGitHub/Principal.java
@@ -1,9 +1,15 @@
 package proyectoGitHub;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Introduce un número:");
+	        int numero = scanner.nextInt();
+	        Clase_Programador1.encontrarDivisores(numero);     
 	}
 
 }
